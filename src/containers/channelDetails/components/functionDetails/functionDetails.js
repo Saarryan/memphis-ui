2c28860e65c661796286580de8d23f481a57e3e1
Sveta Gimpelson <svetagimpelson@Svetas-MacBook-Pro.local>
2022-02-16 11:55:06 +0200

    factory functions final ui changes

--- src/containers/channelDetails/components/functionDetails/functionDetails.js
+++ src/containers/channelDetails/components/functionDetails/functionDetails.js
@@ -31,7 +31,7 @@ function FunctionDetails(props) {
                     </div>
                 </div>}
             </div>
-            <div className="functions-details-footer">
+            {props.chosenFunction && <div className="functions-details-footer">
                 <Button
                     className="modal-btn"
                     width="90px"
@@ -61,52 +61,7 @@ function FunctionDetails(props) {
                     onClick={() => setOpenFunctionForm(true)}
                 />
 
-            </div>
-            {/* {props.chosenFunction && <div>
-                <div className="function-details">
-                    {props.chosenFunction.funcImg ? <img src={props.chosenFunction.funcImg} alt="function" width="50" height="50" className="img-placeholder" /> : <div className="img-placeholder" />}
-                    <div>
-                        <p className="function-name">{props.chosenFunction.funcName}</p>
-                        <p className="data-type">Data type: {props.chosenFunction.inputDataType}</p>
-                    </div>
-                </div>
-                <div className="func-description">{props.chosenFunction.funcDesc}</div>
-                <p className="visit-hub">Visit hub</p>
-                <div className="btn-footer">
-                    <div className="btn-section">
-                        <Button
-                            className="modal-btn"
-                            width="90px"
-                            height="32px"
-                            placeholder={isInstalled ? "Uninstall" : "Install"}
-                            colorType={isInstalled ? "darkPurple" : "lightPurple"}
-                            backgroundColorType={isInstalled ? "none" : "darkPurple"}
-                            border={isInstalled ? "darkPurple" : null}
-                            radiusType="circle"
-                            fontSize="14px"
-                            fontWeight="bold"
-                            aria-haspopup="true"
-                            onClick={() => setInstall(!isInstalled)}
-                        />
-                        <Button
-                            className="modal-btn"
-                            width="90px"
-                            height="32px"
-                            placeholder={"Use"}
-                            colorType="lightPurple"
-                            radiusType="circle"
-                            backgroundColorType={"darkPurple"}
-                            fontSize="14px"
-                            fontWeight="bold"
-                            aria-haspopup="true"
-                            disabled={!isInstalled}
-                            onClick={() => setOpenFunctionForm(true)}
-                        />
-                    </div>
-                </div>
-            </div>
-            } */}
-
+            </div>}
         </div>
     );
 }
